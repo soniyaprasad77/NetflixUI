dbc3dbab3c420eb037722279a96a866dd0fe0521
Your Name <soniyagupta82270@gmail.com>
2024-08-28 01:55:14 +0530

    feat: Improve VideoTitle component layout and add play and info buttons

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -2,9 +2,19 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <h2>{overview}</h2>
+    <div className="pt-28 ml-12 w-1/4">
+      <h1 className="text-5xl font-bold ">{title}</h1>
+      <h2 className="text-xl mt-6">{overview}</h2>
+      <div className="flex gap-2 justify-between mt-6">
+        <button className="bg-gray-200 py-4 whitespace-nowrap w-44 rounded-lg">
+          {" "}
+          ▶ Play
+        </button>
+        <button className="bg-gray-200 py-4 whitespace-nowrap w-44 rounded-lg">
+          {" "}
+          ℹ️ More Info
+        </button>
+      </div>
     </div>
   );
 };
--- src/hooks/useNowPlayingMoviesCallApi.js
+++ src/hooks/useNowPlayingMoviesCallApi.js
@@ -8,7 +8,7 @@ const useNowPlayingMoviesCallApi = () => {
   useEffect(() => {
     const getNowPlayingMovies = async () => {
       const data = await fetch(
-        "https://api.themoviedb.org/3/movie/now_playing?page=1",
+        "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1",
         API_OPTIONS
       );
       const jsonData = await data.json();
