4a11896ee77e18229f14790b88b507191f2e2602
Your Name <soniyagupta82270@gmail.com>
2024-09-14 01:03:42 +0530

    feat:  configure gpt store

--- src/store/appStore.js
+++ src/store/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieSlice from "./movieSlice";
 import userSlice from "./userSlice";
+import gptSlice from "./gptSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userSlice,
     movies: movieSlice,
+    gptsearch: gptSlice,
   },
 });
 
