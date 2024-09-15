34da8d49003048624b010e510a183b58614f57e9
Your Name <soniyagupta82270@gmail.com>
2024-09-16 01:07:47 +0530

    Add language constants for search form

---
+++ src/utils/langConst.js
@@ -0,0 +1,14 @@
+const lang = {
+  en: {
+    search: "Search",
+    searchPlaceholder: "What would you like to watch today?",
+  },
+  hindi: {
+    search: "खोजें",
+    searchPlaceholder: "आप आज क्या देखना चाहेंगे?",
+  },
+  spanish: {
+    search: "Buscar",
+    searchPlaceholder: "¿Qué te gustaría ver hoy?",
+  },
+};
