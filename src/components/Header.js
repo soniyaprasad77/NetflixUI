12cf304cb6389fb1388d3e1472c9e9f03873f78d
Your Name <soniyagupta82270@gmail.com>
2024-09-17 03:46:30 +0530

    Add language selection dropdown to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { addUser, removeUser } from "../store/userSlice";
 import { toggleGPTSearchView } from "../store/gptSlice";
-
+import { SUPPORTED_LANGUAGES } from "../utils/constants";
 import { LOGO } from "../utils/constants";
 import { auth } from "../utils/firebase";
-import GPTSearch from "./GPTSearch";
+import { changeLanguage } from "../store/langSlice";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -57,11 +57,27 @@ const Header = () => {
   const handleGPTSearchClick = () => {
     dispatch(toggleGPTSearchView());
   };
+  const handleLanguageChange = (e) => {
+    dispatch(changeLanguage(e.target.value));
+  };
   return (
     <div className="w-full absolute top-0 left-0 z-20 px-4 py-2 bg-gradient-to-b from-black flex justify-between">
       <img className="w-44" src={LOGO} alt="Netflix Logo" />
       {user.email && (
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center gap-4">
+          <select
+            onClick={handleLanguageChange}
+            className="p-2 bg-slate-600 text-white"
+          >
+            {SUPPORTED_LANGUAGES.map((lang) => {
+              console.log(lang.identifier);
+              return (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              );
+            })}
+          </select>
           <button
             onClick={handleGPTSearchClick}
             className="bg-purple-400 text-white rounded-lg px-4 py-2 my-2 mr-4"
