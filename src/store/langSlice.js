2fd577d6c9addf9c0fd4a0c371638c84326ddd12
Your Name <soniyagupta82270@gmail.com>
2024-09-16 17:22:24 +0530

    setup initial store for multi language support

---
+++ src/store/langSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+const langSlice = createSlice({
+  name: "language",
+  initialState: {},
+  reducers: {
+    
+  },
+});
+
+export default langSlice.reducer;
