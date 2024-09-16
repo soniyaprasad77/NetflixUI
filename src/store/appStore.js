21721dcecf535232523e4296a3ff48d6dd097cf5
Your Name <soniyagupta82270@gmail.com>
2024-09-17 03:31:43 +0530

    Add language slice to app store configuration

--- src/store/appStore.js
+++ src/store/appStore.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import movieSlice from "./movieSlice";
 import userSlice from "./userSlice";
 import gptSlice from "./gptSlice";
+import langSlice from "./langSlice";
 
 const appStore = configureStore({
   reducer: {
     user: userSlice,
     movies: movieSlice,
     gpt: gptSlice,
+    language: langSlice,
   },
 });
 
--- src/store/langSlice.js
+++ src/store/langSlice.js
@@ -1,10 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 const langSlice = createSlice({
   name: "language",
-  initialState: {},
+  initialState: {
+    language: "en",
+  },
   reducers: {
-    
+    changeLanguage: (state, action) => {
+      state.language = action.payload;
+    },
   },
 });
+
+export const { changeLanguage } = langSlice.actions;
 
 export default langSlice.reducer;
