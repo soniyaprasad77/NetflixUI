8d770272ead86e6628ae8a11f18e6452f2a826b6
Your Name <soniyagupta82270@gmail.com>
2024-10-10 15:20:54 +0530

    bug fix: movie redirect page

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -5,6 +5,7 @@ import useUpcomingMovies from "../hooks/useUpcomingMovies";
 import { useSelector } from "react-redux";
 import GeminiSearchPage from "./GeminiSearchPage";
 import { Outlet } from "react-router-dom"; // Import Outlet
+import { useLocation } from "react-router-dom";
 
 // Components
 import Header from "./Header";
@@ -19,16 +20,17 @@ const Browse = () => {
   useUpcomingMovies();
 
 
+  const location = useLocation(); // Import and use useLocation hook from react-router-dom
+
   return (
     <div className='relative'>
       <Header />
-      {location.pathname === "/browse" && (
+      {location.pathname === "/browse" || location.pathname === "/browse/" ? (
         <>
           {geminiSearch ? <MainContainer /> : <GeminiSearchPage />}
         </>
-      )}
-      <Outlet /> {/* This will render the nested routes */}
-
+      ) : <Outlet />}
+      {/* This will render the nested routes */}
     </div>
   );
 };
